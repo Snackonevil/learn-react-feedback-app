22d784a27306a2de9e34cddcafcdd9672571c74c
Kevin Lacson <lacsonky@gmail.com>
2022-01-03 19:42:22 -0500

    addFeedback function

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,48 +1,59 @@
-import { useState } from 'react';
-import Card from './shared/Card';
-import Button from './shared/Button';
-import RatingSelect from './RatingSelect';
+import { useState } from "react";
+import Card from "./shared/Card";
+import Button from "./shared/Button";
+import RatingSelect from "./RatingSelect";
 
-function FeedbackForm() {
-  const [text, setText] = useState('');
-  const [btnDisabled, setBtnDisabled] = useState(true);
-  const [message, setMessage] = useState('');
-  const [rating, setRating] = useState(10);
+function FeedbackForm({ handleAdd }) {
+    const [text, setText] = useState("");
+    const [btnDisabled, setBtnDisabled] = useState(true);
+    const [message, setMessage] = useState("");
+    const [rating, setRating] = useState(10);
 
-  const handleTextChange = e => {
-    if (text === '') {
-      setBtnDisabled(true);
-      setMessage(null);
-    } else if (text !== '' && text.trim().length < 10) {
-      setMessage('Text must be at least 10 characters');
-      setBtnDisabled(true);
-    } else {
-      setMessage(null);
-      setBtnDisabled(false);
-    }
-    setText(e.target.value);
-  };
+    const handleTextChange = e => {
+        if (text === "") {
+            setBtnDisabled(true);
+            setMessage(null);
+        } else if (text !== "" && text.trim().length < 10) {
+            setMessage("Text must be at least 10 characters");
+            setBtnDisabled(true);
+        } else {
+            setMessage(null);
+            setBtnDisabled(false);
+        }
+        setText(e.target.value);
+    };
 
-  return (
-    <Card>
-      <form>
-        <h2>How would you rate your service with us?</h2>
-        <RatingSelect select={rating => setRating(rating)} />
-        <div className="input-group">
-          <input
-            value={text}
-            onChange={handleTextChange}
-            type="text"
-            placeholder="Write a review"
-          ></input>
-          <Button type="submit" isDisabled={btnDisabled}>
-            Send
-          </Button>
-        </div>
-        {message && <div className="message">{message}</div>}
-      </form>
-    </Card>
-  );
+    const handleSubmit = e => {
+        e.preventDefault();
+        if (text.trim().length >= 10) {
+            const newFeedback = {
+                text,
+                rating,
+            };
+            handleAdd(newFeedback);
+        }
+    };
+
+    return (
+        <Card>
+            <form onSubmit={handleSubmit}>
+                <h2>How would you rate your service with us?</h2>
+                <RatingSelect select={rating => setRating(rating)} />
+                <div className="input-group">
+                    <input
+                        value={text}
+                        onChange={handleTextChange}
+                        type="text"
+                        placeholder="Write a review"
+                    ></input>
+                    <Button type="submit" isDisabled={btnDisabled}>
+                        Send
+                    </Button>
+                </div>
+                {message && <div className="message">{message}</div>}
+            </form>
+        </Card>
+    );
 }
 
 export default FeedbackForm;
