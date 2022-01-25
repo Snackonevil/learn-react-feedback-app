59a86b3897aab5817b5cb35b43f01e8addbb16eb
Kevin Lacson <lacsonky@gmail.com>
2022-01-24 20:48:45 -0500

    linked title to home

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,28 +1,31 @@
-import PropTypes from 'prop-types';
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
 
 function Header({ text, bgColor, textColor }) {
-  const headerStyles = {
-    backgroundColor: bgColor,
-    color: textColor,
-  };
+    const headerStyles = {
+        backgroundColor: bgColor,
+        color: textColor,
+    };
 
-  return (
-    <header style={headerStyles}>
-      <div className="container">
-        <h3>{text}</h3>
-      </div>
-    </header>
-  );
+    return (
+        <Link to="/">
+            <header style={headerStyles}>
+                <div className="container">
+                    <h3>{text}</h3>
+                </div>
+            </header>
+        </Link>
+    );
 }
 
 Header.defaultProps = {
-  text: 'Feedback UI',
-  bgColor: 'rgba(0,0,0,0.4)',
-  textColor: '#ff6a95',
+    text: "Feedback UI",
+    bgColor: "rgba(0,0,0,0.4)",
+    textColor: "#ff6a95",
 };
 
 Header.propTypes = {
-  text: PropTypes.string,
+    text: PropTypes.string,
 };
 
 export default Header;
