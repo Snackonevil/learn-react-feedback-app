f84bd073a64b8e60cc54be570c38a206adaa6737
Kevin Lacson <lacsonky@gmail.com>
2022-01-25 18:19:39 -0500

    successful post request from ui

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,5 +1,4 @@
 import { createContext, useState, useEffect } from "react";
-import { v4 as uuidv4 } from "uuid";
 
 const FeedbackContext = createContext();
 
@@ -28,9 +27,17 @@ export const FeedbackProvider = ({ children }) => {
     };
 
     // Add feedback
-    const addFeedback = newFeedback => {
-        newFeedback.id = uuidv4();
-        setFeedback([newFeedback, ...feedback]);
+    const addFeedback = async newFeedback => {
+        const response = await fetch("http://localhost:5000/feedback", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(newFeedback),
+        });
+        const data = await response.json();
+
+        setFeedback([data, ...feedback]);
     };
 
     // Delete feedback
