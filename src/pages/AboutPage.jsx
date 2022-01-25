af44b758c4a515bc70d6b3c5e87c58297b8c9d40
Kevin Lacson <lacsonky@gmail.com>
2022-01-24 20:46:58 -0500

    built and linkked about page with react router dom

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,5 +1,26 @@
+import { Link } from "react-router-dom";
+import Card from "../components/shared/Card";
+
 function AboutPage() {
-    return <div>About</div>;
+    return (
+        <Card>
+            <div className="about">
+                <h1>About This Project</h1>
+                <p>
+                    This is a React app to leave feedback for a product or
+                    service
+                </p>
+                <p>
+                    This app was built alongside Brad Traversy's 'React Front to
+                    Back 2022' course on Udemy.com
+                </p>
+                <p>Thank you, Brad!</p>
+                <p>
+                    <Link to="/">Back to Home</Link>
+                </p>
+            </div>
+        </Card>
+    );
 }
 
 export default AboutPage;
